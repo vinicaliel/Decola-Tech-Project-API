444f567f58fd4e9d30b401d4aca0ac181d2a4ae0
Caliel Passos <caliel1023@yahoo.com>
2025-03-18 12:15:01 -0300

    My first commit

---
+++ src/main/java/me/dio/Decola_Tech_API_Project/DecolaTechApiProjectApplication.java
@@ -0,0 +1,14 @@
+package me.dio.Decola_Tech_API_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechApiProjectApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/me/dio/Decola_Tech_API_Project/DecolaTechApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Decola_Tech_API_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
